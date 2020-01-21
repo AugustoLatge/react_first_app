07a6fb4a7877dd74425234e682ce63b4fc93e402
Augusto <augustoslatge@gmail.com>
2020-01-21 16:07:56 -0500

    first way of using context component

--- react-first-app/src/components/Persons/Persons.js
+++ react-first-app/src/components/Persons/Persons.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
-
 class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state) {
   //   console.log('[Persons.js] getDerivedStateFromProps');
@@ -53,6 +52,7 @@ class Persons extends PureComponent {
             name={person.name}
             age={person.age}                  
             changed={event => this.props.changed(event, person.id)}
+            isAuth={this.props.isAuthenticated}
           />
         // </ErrorBoundary>
       );
--- react-first-app/src/containers/App.js
+++ react-first-app/src/containers/App.js
@@ -5,6 +5,7 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 import withClass from "../hoc/withClass";
 import Aux from "../hoc/Auxilliary";
+import AuthContext from '../context/auth-context';
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +22,8 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -79,6 +81,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({authenticated: true});
+  };
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -88,19 +94,35 @@ class App extends Component {
         persons={this.state.persons}
         clicked={this.deletePersonHandler}
         changed={this.nameChangeHandler}
+        isAuthenticated={this.state.authenticated}
       />;
     }
 
     return (
       <Aux>
-        <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
-        {this.state.showCockpit ? <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          personsLength={this.state.persons.length}
-          clicked={this.togglePersonsHandler}
-        /> : null}
-        {persons}
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove Cockpit
+        </button>
+        <AuthContext.Provider
+          value={{
+            authenticated: this.state.authenticated,
+            login: this.loginHandler
+          }}
+        >
+          {this.state.showCockpit ? (
+            <Cockpit
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons}
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler}
+            />
+          ) : null}
+          {persons}
+        </AuthContext.Provider>
       </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')
